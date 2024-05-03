354c695debc71e5e7cbfb87c89f3de6ee2418c18
jl-kammii <thomasjouglet2002@gmail.comm>
2024-05-03 10:41:52 +0200

    back

--- controller/Music.js
+++ controller/Music.js
@@ -1,41 +1,44 @@
-const { error } = require("console");
-const data = require("./../model/data.json");
-const controllerMusic = {
-    find: (req,res) => {
-        if(req.query.search){
+//const data = require("./../models/data.json");
+const Music = require("../model/Music");
+const controllerMusic =
+{
+    find: async (req, res) => {
+        if (req.query.search) {
             const query = req.query.search;
             const result = data.filter(song => song.title.toLowerCase().includes(query.toLowerCase()));
-            if(result.length === 0) {
-                return res.status(404).json({error: "Not found"});
-            } else {
-                return res.status(200).json({result})
+            if (result.length === 0) {
+                return res.status(404).json({ error: "No matching songs found." });
             }
+            res.status(200).json({ result });
+            console.log(result);
         } else {
-            res.status(200).json({result: data});
+            const data = await Music.findAll();
+            res.status(200).json({ result: data });
         }
     },
 
-    create: (req,res)=>{
+    create: async (req, res) => {
         console.log(req.body);
-        res.status(201).json({message: "Votre musique a été ajoutée",data: req.body});
+        const data = await Music.create(req.body);
+        console.log(data);
+        res.status(201).json({ message: "votre musique a été ajoutée", data: req.body });
     },
 
-    findById: (req,res) => {
-        const id = req.params.id;
-        if(isNaN(id)){
-            res.status(405).json({error: "Param is not a number"});
+    findById: (req, res) => {
+        const id = req.params.id
+        if (isNaN(id)) {
+            return res.status(400).json({ error: "Don't use text for id" });
         }
-        if(id>data.length){
-            res.status(404).json({error: "Music not found"});
+        if (id > data.length) {
+            return res.status(404).json({ error: "ID not found ..." });
         }
-        res.status(200).json({result: data[id-1]});
+
+        return res.status(200).json({ result: data[id - 1] })
     },
 
-    //Créer la méthode pour renvoyer une musique au hasard
-    random: (req,res) => {
-        const music = data[Math.floor(Math.random()*data.length)];
-        res.status(200).json({result: music});
+    random: (req, res) => {
+
     }
-};
+}
 
-module.exports = controllerMusic;
+module.exports = controllerMusic
