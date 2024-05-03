6eff68b8c3b754680fbbd0fd67a988adcfc17487
jl-kammii <thomasjouglet2002@gmail.comm>
2024-05-03 13:31:38 +0200

    swagger

--- serveur.js
+++ serveur.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json'); // Importez votre fichier JSON Swagger ici
+const swaggerJS = require("swagger-jsdoc");
+const option = swaggerJS(swaggerDocument);
 const port = 3000;
 const version = 'v1';
 const router = require('./routes/routes');
@@ -14,7 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(`/api/${version}`, router);
 
 // Middleware pour afficher la documentation Swagger
-app.use(`/api-docs`, swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use(`/api-docs`, swaggerUi.serve, swaggerUi.setup(option));
 
 db.sync().then(() => {
   console.log('DBConnect est synchronisé')
